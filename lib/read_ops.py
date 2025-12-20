291a9b43669a601513ae82ef03177c2957c3ac40
jyotsna70044-jpg <jyotsna70044@gmail.com>
2025-12-21 00:23:47 +0530

    add code

--- lib/read_ops.py
+++ lib/read_ops.py
@@ -12,7 +12,7 @@ def read_from_s3_in_csv(glue_context, source_s3_path):
         connection_type="s3",
         connection_options={"paths": [source_s3_path]},
         format="csv",  # Example: 'csv', 'json', 'parquet'
-        format_options={"withHeader": True, "separator": ","} if format == "csv" else {},  # Example for CSV with header
+        format_options={"withHeader": True, "separator": ","} if format == "csv" else {},
         transformation_ctx="datasource0"
     )
     return dynamic_frame
@@ -23,7 +23,7 @@ def read_from_s3_in_par(glue_context, source_s3_path):
         connection_type="s3",
         connection_options={"paths": [source_s3_path]},
         format="parquet",  # Example: 'csv', 'json', 'parquet'
-        # not required# format_options={"withHeader": True, "separator": ","} if format == "csv" else {},  # Example for CSV with header
+        # not required# format_options={"withHeader": True, "separator": ","} if format == "csv" else {},
         transformation_ctx="datasource0"
     )
     return dynamic_frame
