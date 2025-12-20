a3e09acdc8799f70b2511c1264880b20ab2d3efc
jyotsna70044-jpg <jyotsna70044@gmail.com>
2025-12-21 01:30:47 +0530

    add code

--- ssp-glue-clean-job.py
+++ ssp-glue-clean-job.py
@@ -7,7 +7,7 @@ from awsglue.job import Job
 from awsglue.dynamicframe import DynamicFrame
 from lib.clean import clean_data
 from lib.save_ops import write_in_s3_in_par
-from lib.read_ops import read_from_s3_in_csv
+from lib.read_ops import read_from_catalog,read_from_s3_in_csv
 
 ## @params: [JOB_NAME]
 args = getResolvedOptions(sys.argv, ['JOB_NAME', 'S3_SOURCE_PATH', 'S3_TARGET_PATH'])
@@ -24,7 +24,10 @@ print(source_s3_path)
 target_s3_path = args['S3_TARGET_PATH'] if 'S3_TARGET_PATH' in args else "s3://your-target-bucket/output-folder/"
 print(target_s3_path)
 # ✅Step 1: Read data using s3 in csv
-dynamic_frame = read_from_s3_in_csv(glueContext, source_s3_path)
+database_name = "ssp"
+table_name = "bronze"
+dynamic_frame = read_from_catalog(glueContext, database_name, table_name)
+# dynamic_frame = read_from_s3_in_csv(glueContext, source_s3_path)
 dynamic_frame.printSchema()
 # step 2: Convert DynamicFrame to Spark DataFrame for transformations
 df = dynamic_frame.toDF()
